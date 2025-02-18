dcc09d9c4a64e21cd9a7248aad0ad4d29a441cd4
Alisson Lamarque <alissonlamarque.si@gmail.com>
2025-02-18 01:57:19 -0300

    aperfeiçoando movimentação inimigo

--- scripts/demon.gd
+++ scripts/demon.gd
@@ -1,18 +1,33 @@
 extends CharacterBody2D
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var player: CharacterBody2D = $"../Player"
-@export var speed: float = 100
+
+@export var speed: float = 70
 @export var stop_distance: float = 10
 
-func _physics_process(delta: float) -> void:
-	if player:
-		var distance = global_position.distance_to(player.global_position)
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var player = get_parent().get_node("Player")
 
-		if distance > stop_distance: 
-			var direction = (player.global_position - global_position).normalized()
-			velocity = direction * speed
-			move_and_slide()
-			look_at(player.global_position)
+var playerPos
+var targetPos
+
+func _physics_process(delta: float) -> void:
+	playerPos = player.position
+	targetPos = (playerPos - position).normalized()
+	
+	if position.distance_to(playerPos) > stop_distance:
+		position += targetPos * speed * delta
+		update_animation(targetPos)
+	else:
+		animated_sprite.play("idle")
+
+func update_animation(direction: Vector2) -> void:
+	if abs(direction.x) > abs(direction.y):
+		if direction.x > 0:
+			animated_sprite.play("run_right")
 		else:
-			velocity = Vector2.ZERO
+			animated_sprite.play("run_left")
+	else:
+		if direction.y > 0:
+			animated_sprite.play("run_down")
+		else:
+			animated_sprite.play("run_up")
