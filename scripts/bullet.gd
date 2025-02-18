83bb5d7a4212ab786d786bcc1b09afc4c1184fae
Gabriel <gabriel@gabriel.com>
2025-02-17 22:06:57 -0300

    player andando e cajado atirando e particulas e varias coisas

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -4,17 +4,10 @@ extends Area2D
 var direction = Vector2.ZERO
 
 func _ready():
-	# Inicia a animação do projétil (ajuste o nome se necessário)
-	$AnimatedSprite2D.play("fly")
+	$AnimatedSprite2D.play("default")
 
-func _process(delta):
-	# Move o projétil na direção especificada
+func _physics_process(delta: float):
 	position += direction * speed * delta
-	
-	# Remove o projétil se sair da tela
-	if not get_viewport_rect().has_point(global_position):
-		queue_free()
 
 func _on_body_entered(body):
-	# Se o projétil atingir algo, ele é removido
 	queue_free()
