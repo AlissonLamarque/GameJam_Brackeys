55411032ff052e547d51e15d9eaa122d4f0cbee7
Gabriel <gabriel@gabriel.com>
2025-02-18 01:26:04 -0300

    nao lembro oq q eu fiz

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,13 +2,17 @@ extends CharacterBody2D
 
 const speed = 150.0
 
+@onready var camera = get_parent().get_node("Camera2D")
+
 var rng = RandomNumberGenerator.new()
 
 var health = 100
 var player_alive = true
 var is_moving = false
 
-@export var staff_distance = 5
+const crosshair_distance = 20
+
+@export var staff_distance = 1
 @export var bullet_scene: PackedScene
 var can_shoot = true
 
@@ -26,6 +30,7 @@ func get_look_direction():
 
 func _ready():
 	initial_light_scale = $Staff/PointLight2D.scale
+	
 
 func _process(delta):
 	if player_alive:
@@ -42,18 +47,20 @@ func rotate_staff(delta):
 	var angle = get_look_direction()
 	
 	if angle.x > 0:
-		$Staff/Sprite2D.flip_h = false
+		$Staff/Sprite2D.flip_v = false
 		light.position = $Staff/Muzzle.position
 		
 	else:
-		$Staff/Sprite2D.flip_h = true
+		$Staff/Sprite2D.flip_v = true
 		light.position = $Staff/Muzzle2.position
 		
 	
 	staff_position_offset = lerp(staff_position_offset, staff_position_offset.move_toward(staff_position_target, delta), 0.7)
 	
+	var interval = 2
+	
 	if staff_position_offset.is_equal_approx(staff_position_target):
-		staff_position_target = Vector2(rng.randf_range(-1.5, 1.5), rng.randf_range(-1.5, 1.5))
+		staff_position_target = Vector2(rng.randf_range(-interval, interval), rng.randf_range(-interval, interval))
 	
 	$Staff.position = (angle * staff_distance) + staff_position_offset
 	
@@ -62,19 +69,21 @@ func rotate_staff(delta):
 func shoot():
 	can_shoot = false
 	
-	$Staff/PointLight2D.scale = initial_light_scale * 2
+	$Staff/PointLight2D.scale = initial_light_scale * 2.5
 	
 	var bullet = bullet_scene.instantiate()
 	
 	var muzzle = $Staff/Muzzle
 	
-	if $Staff/Sprite2D.flip_h:
+	if $Staff/Sprite2D.flip_v:
 		muzzle = $Staff/Muzzle2
 	
 	bullet.global_position = muzzle.global_position
 	bullet.direction = (get_global_mouse_position() - muzzle.global_position).normalized()
 	
 	get_parent().add_child(bullet)
+	
+	camera.apply_shake(0.5, 2)
 
 	await get_tree().create_timer(0.2).timeout
 
@@ -87,7 +96,7 @@ func _physics_process(delta: float):
 	
 	var light = $Staff/PointLight2D
 	
-	light.scale = lerp(light.scale, initial_light_scale, 0.1)
+	light.scale = lerp(light.scale, initial_light_scale, 0.07)
 	
 	if health <= 0:
 		player_alive = false
