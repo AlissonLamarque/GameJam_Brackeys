cb297a53f5674072735f83df5873668d48fab70a
Gabriel <gabriel@gabriel.com>
2025-02-21 21:04:18 -0300

    Mesa de crafting e respawn dos itens funcionando

--- scripts/items/bottle.gd
+++ scripts/items/bottle.gd
@@ -8,7 +8,7 @@ extends Bola
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	item_nome = "bottle"
-	pass # Replace with function body.
+	is_base = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/items/golden_feather.gd
+++ scripts/items/golden_feather.gd
@@ -1,12 +1,6 @@
 extends Bola
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	item_nome = "golden_feather"
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	is_base = true
