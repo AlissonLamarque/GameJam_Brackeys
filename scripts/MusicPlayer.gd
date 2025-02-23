fc1477e6b0437aec6de7d1ed2f6d7a2ceb419130
JP-Righi <jprighi@inf.ufsm.br>
2025-02-23 07:09:07 -0300

    ajsutei volumes

--- scripts/MusicPlayer.gd
+++ scripts/MusicPlayer.gd
@@ -3,5 +3,5 @@ extends AudioStreamPlayer
 
 func _ready():
 	stream = load("res://assets/sounds/MusicaBrackeysGameJogoJam.wav")  # Substitua pelo caminho correto
-	volume_db = -10  # Ajuste o volume se necessário
+	volume_db = 1  # Ajuste o volume se necessário
 	play()  # Começa a tocar
