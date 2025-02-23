f719ac975330baacb7017ebadeb9eca25bacac8c
Alisson Lamarque <alissonlamarque.si@gmail.com>
2025-02-23 05:50:13 -0300

    Finalização dos menus

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,26 +2,20 @@ extends Node2D
 
 
 func _on_play_button_pressed() -> void:
-	print("Start Game")
 	# Troca para a cena do jogo
-	
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
 func _on_config_button_pressed() -> void:
-	print("Configs")
 	# Troca para a cena de configurações
-	
 	get_tree().change_scene_to_file("res://scenes/configs.tscn")
 
 
 func _on_credits_button_pressed() -> void:
-	print("Credits")
 	# Troca para a cena de créditos
-	
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
 
 func _on_exit_button_pressed() -> void:
-	print("Quit")
+	# Termina o jogo
 	get_tree().quit()
